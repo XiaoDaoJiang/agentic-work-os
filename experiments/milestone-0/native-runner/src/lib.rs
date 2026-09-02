01910b9536a964e2734656847d6ecace431d63f1
XiaoDaoJiang <38294800+XiaoDaoJiang@users.noreply.github.com>
2026-09-02 14:51:32 +0800

    style(m0): scope non-Windows ptr-arg allowance

--- experiments/milestone-0/native-runner/src/lib.rs
+++ experiments/milestone-0/native-runner/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod capabilities;
 pub mod event_writer;
 pub mod hostile_evidence;
+#[cfg_attr(not(windows), allow(clippy::ptr_arg))]
 pub mod hostile_probe;
 pub mod protocol;
 pub mod windows_process_truth;
